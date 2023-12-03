6c50d3dc496739b513be9ad7645e57150c5ec2be
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2023-12-04 05:32:11 +0800

    publish update

--- js/getTiket.js
+++ js/getTiket.js
@@ -38,13 +38,13 @@ export const tableEvent = `
 </td>
 <td class="px-6 py-3 whitespace-nowrap">
 <a
-      href="updatetiket?obatId=#IDEDIT#"
+      href="updatetiket?tiketId=#IDEDIT#"
       class="inline-flex items-center px-2 cursor-pointer text-sm text-green-600 decoration-2 hover:underline font-medium"
     >
       Edit
  </a>
   
-    <button class="btn btn-outline-danger btn-sm" onclick="deleteObat('#IDHAPUS#')">Delete</button>
+    <button class="btn btn-outline-danger btn-sm" onclick="deleteTiket('#IDHAPUS#')">Delete</button>
   </td>
 </td>
 </tr>

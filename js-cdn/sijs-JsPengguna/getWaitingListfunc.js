48c5a07b6cbe1baa5c7e4fe7a4ca6cd39d9ad86d
Shahieza Alfadia Shauqie <shahiezasauki15@gmail.com>
2024-01-11 04:23:40 +0700

    update

--- js-cdn/sijs-JsPengguna/getWaitingListfunc.js
+++ js-cdn/sijs-JsPengguna/getWaitingListfunc.js
@@ -1,4 +1,4 @@
-import { URLGetEvent, responseData } from "./getWaitingList.js";
+import { URLGetEvent, responseData } from "https://cdn.jsdelivr.net/gh/serbaevents/cdnJS@0.0.5/js-cdn/sijs-JsPengguna/getWaitingList.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 import { hide } from "https://jscroot.github.io/element/croot.js";
 
